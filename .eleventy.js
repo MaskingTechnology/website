b274b4777cbc9eb8f623aa2e1fa6a04aaa7887c0
Bas Meeuwissen <bas@masking.tech>
2025-10-10 17:01:36 +0200

    added slides to the downloads

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig)
     eleventyConfig.addPassthroughCopy("src/images");
     eleventyConfig.addPassthroughCopy("src/fonts");
     eleventyConfig.addPassthroughCopy("src/js");
+    eleventyConfig.addPassthroughCopy("src/downloads");
 
       return {
           dir: 
